7c3175b61b47a92be47f2cca7754d53319c7d40c
Dominik Schwabe <schwabedominik@gmail.com>
2019-10-22 01:01:09 +0200

    prototype which enables propper data exchange between frontend and backend

--- application/app/forms.py
+++ application/app/forms.py
@@ -1,15 +1,19 @@
 from flask import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import SelectField, FileField
 from wtforms.validators import DataRequired
 
 
 
 class FilesSubmitForm(FlaskForm):
-    file1 = StringField('file1', validators=[DataRequired()])
-    file2 = StringField('file2', validators=[DataRequired()])
+    file_left = SelectField("file_left", validators=[DataRequired()])
+    file_right = SelectField("file_right", validators=[DataRequired()])
 
     def __init__(self, *args, **kwargs):
-        kwargs['formdata'] = request.args
-        kwargs['csrf_enabled'] = False
         super(FilesSubmitForm, self).__init__(*args, **kwargs)
+
+class FileUploadForm(FlaskForm):
+    file = FileField("file_upload", validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        super(FileUploadForm, self).__init__(*args, **kwargs)
--- application/app/routes.py
+++ application/app/routes.py
@@ -1,16 +1,40 @@
-from flask import render_template
-from flask import jsonify
+from flask import render_template, redirect, jsonify, url_for
 from app import app
-from app.forms import FilesSubmitForm
+from app.forms import FilesSubmitForm, FileUploadForm
 
 
+left_docs = {}
+right_docs = {}
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    form = FilesSubmitForm()
-    if form.validate():
-        file1 = form.file1.data
-        file2 = form.file2.data
-        return render_template('metrics.html', form=form)
-    else:
-        return render_template('index.html', form=form)
+    form_choice = FilesSubmitForm()
+    form_upload_left = FileUploadForm(prefix="form_left")
+    form_upload_right = FileUploadForm(prefix="form_right")
+
+    form_choice.file_left.choices = [(key,key) for key in left_docs.keys()]
+    form_choice.file_right.choices = [(key,key) for key in right_docs.keys()]
+    if form_choice.validate_on_submit():
+        file_left = left_docs[form_choice.file_left.data]
+        file_right = right_docs[form_choice.file_right.data]
+        results = (file_left, file_right)
+        return render_template("index.html", form_choice=form_choice, form_upload_left=form_upload_left, form_upload_right=form_upload_right, results=results)
+    if form_upload_left.validate_on_submit():
+        filename = form_upload_left.file.data.filename
+        filecontent = form_upload_left.file.data.read().decode("utf-8")
+        left_docs[filename] = filecontent
+    if form_upload_right.validate_on_submit():
+        filename = form_upload_right.file.data.filename
+        filecontent = form_upload_right.file.data.read().decode("utf-8")
+        right_docs[filename] = filecontent
+    form_choice.file_left.choices = [(key,key) for key, value in left_docs.items()]
+    form_choice.file_right.choices = [(key,key) for key, value in right_docs.items()]
+    return render_template("index.html", form_choice=form_choice, form_upload_left=form_upload_left, form_upload_right=form_upload_right, results=None)
+
+
+@app.route("/delete", methods=["GET", "POST"])
+def upload_left():
+    left_docs = {}
+    right_docs = {}
+    return redirect(url_for("index"))
