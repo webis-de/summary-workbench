95efe9fb55e8af985d5d4b698b66f6c5fa6049a5
Dominik Schwabe <schwabedominik@gmail.com>
2019-10-22 22:33:49 +0200

    metrics can be calculated and displayed (exception handling required)

--- application/app/routes.py
+++ application/app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, jsonify, url_for
 from app import app
 from app.forms import FilesSubmitForm, FileUploadForm
+from rouge import Rouge
+from collections import defaultdict
 
 
 left_docs = {}
@@ -18,16 +20,26 @@ def index():
     if form_choice.validate_on_submit():
         file_left = left_docs[form_choice.file_left.data]
         file_right = right_docs[form_choice.file_right.data]
-        results = (file_left, file_right)
-        return render_template("index.html", form_choice=form_choice, form_upload_left=form_upload_left, form_upload_right=form_upload_right, results=results)
+        rouge = Rouge()
+        results = rouge.get_scores(file_left, file_right, avg=True)
+        table = {}
+        thead = [""]
+        tbody = defaultdict(list)
+        for metric, metric_info in results.items():
+            thead.append(metric)
+            for key, value in metric_info.items():
+                tbody[key].append(value)
+        table["thead"] = thead
+        table["tbody"] = tbody
+        return render_template("index.html", form_choice=form_choice, form_upload_left=form_upload_left, form_upload_right=form_upload_right, table=table)
     if form_upload_left.validate_on_submit():
         filename = form_upload_left.file.data.filename
         filecontent = form_upload_left.file.data.read().decode("utf-8")
-        left_docs[filename] = filecontent
+        left_docs[filename] = filecontent.splitlines()
     if form_upload_right.validate_on_submit():
         filename = form_upload_right.file.data.filename
         filecontent = form_upload_right.file.data.read().decode("utf-8")
-        right_docs[filename] = filecontent
+        right_docs[filename] = filecontent.splitlines()
     form_choice.file_left.choices = [(key,key) for key, value in left_docs.items()]
     form_choice.file_right.choices = [(key,key) for key, value in right_docs.items()]
     return render_template("index.html", form_choice=form_choice, form_upload_left=form_upload_left, form_upload_right=form_upload_right, results=None)
@@ -35,6 +47,6 @@ def index():
 
 @app.route("/delete", methods=["GET", "POST"])
 def upload_left():
-    left_docs = {}
-    right_docs = {}
+    left_docs.clear()
+    right_docs.clear()
     return redirect(url_for("index"))
