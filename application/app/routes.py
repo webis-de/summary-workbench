3a01732ee2e30b98307dd3fd424e933a12a94497
Dominik Schwabe <schwabedominik@gmail.com>
2019-10-23 00:45:37 +0200

    exceptions are alerted now

--- application/app/routes.py
+++ application/app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, redirect, jsonify, url_for
+from flask import render_template, redirect, jsonify, url_for, flash
 from app import app
 from app.forms import FilesSubmitForm, FileUploadForm
 from rouge import Rouge
 from collections import defaultdict
+import time
 
 
 left_docs = {}
@@ -15,33 +16,38 @@ def index():
     form_upload_left = FileUploadForm(prefix="form_left")
     form_upload_right = FileUploadForm(prefix="form_right")
 
-    form_choice.file_left.choices = [(key,key) for key in left_docs.keys()]
-    form_choice.file_right.choices = [(key,key) for key in right_docs.keys()]
-    if form_choice.validate_on_submit():
-        file_left = left_docs[form_choice.file_left.data]
-        file_right = right_docs[form_choice.file_right.data]
-        rouge = Rouge()
-        results = rouge.get_scores(file_left, file_right, avg=True)
-        table = {}
-        thead = [""]
-        tbody = defaultdict(list)
-        for metric, metric_info in results.items():
-            thead.append(metric)
-            for key, value in metric_info.items():
-                tbody[key].append(value)
-        table["thead"] = thead
-        table["tbody"] = tbody
-        return render_template("index.html", form_choice=form_choice, form_upload_left=form_upload_left, form_upload_right=form_upload_right, table=table)
-    if form_upload_left.validate_on_submit():
-        filename = form_upload_left.file.data.filename
-        filecontent = form_upload_left.file.data.read().decode("utf-8")
-        left_docs[filename] = filecontent.splitlines()
-    if form_upload_right.validate_on_submit():
-        filename = form_upload_right.file.data.filename
-        filecontent = form_upload_right.file.data.read().decode("utf-8")
-        right_docs[filename] = filecontent.splitlines()
-    form_choice.file_left.choices = [(key,key) for key, value in left_docs.items()]
-    form_choice.file_right.choices = [(key,key) for key, value in right_docs.items()]
+    form_choice.file_left.choices = [(key,key) for key, _ in sorted(left_docs.items(), key=lambda x: x[1][0], reverse=True)]
+    form_choice.file_right.choices = [(key,key) for key, _ in sorted(right_docs.items(), key=lambda x: x[1][0], reverse=True)]
+
+    try:
+        if form_choice.validate_on_submit():
+            file_left = left_docs[form_choice.file_left.data][1]
+            file_right = right_docs[form_choice.file_right.data][1]
+            rouge = Rouge()
+            results = rouge.get_scores(file_left, file_right, avg=True)
+            table = {}
+            thead = [""]
+            tbody = defaultdict(list)
+            for metric, metric_info in results.items():
+                thead.append(metric)
+                for key, value in metric_info.items():
+                    tbody[key].append(value)
+            table["thead"] = thead
+            table["tbody"] = tbody
+            return render_template("index.html", form_choice=form_choice, form_upload_left=form_upload_left, form_upload_right=form_upload_right, table=table)
+        if form_upload_left.validate_on_submit():
+            filename = form_upload_left.file.data.filename
+            filecontent = form_upload_left.file.data.read().decode("utf-8")
+            left_docs[filename] = (time.time(), filecontent.splitlines())
+        if form_upload_right.validate_on_submit():
+            filename = form_upload_right.file.data.filename
+            filecontent = form_upload_right.file.data.read().decode("utf-8")
+            right_docs[filename] = (time.time(), filecontent.splitlines())
+    except Exception as e:
+        flash(str(e))
+
+    form_choice.file_left.choices = [(key,key) for key, _ in sorted(left_docs.items(), key=lambda x: x[1][0], reverse=True)]
+    form_choice.file_right.choices = [(key,key) for key, _ in sorted(right_docs.items(), key=lambda x: x[1][0], reverse=True)]
     return render_template("index.html", form_choice=form_choice, form_upload_left=form_upload_left, form_upload_right=form_upload_right, results=None)
 
 
--- application/config.py
+++ application/config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY') or ''
-    WTF_CSRF_ENABLED = False                  # protect against csrf-attack
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dljfaksdjf lkasd'
     BOOTSTRAP_USE_MINIFIED = False
     BOOTSTRAP_SERVE_LOCAL = True
