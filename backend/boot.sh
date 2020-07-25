cb14c775963ab254d10296df46a407a3cfb6e695
Dominik Schwabe <schwabedominik@gmail.com>
2020-07-25 17:27:54 +0200

    fixed develop, use of lock files

--- backend/boot.sh
+++ backend/boot.sh
@@ -9,7 +9,7 @@ else
 fi
 
 if [ "$INSTALL_PACKAGES" = "yes" ]; then
-    pipenv install --skip-lock         # install packages
+    pipenv sync                        # install packages
 fi
 
 pipenv run python setup.py             # download models
--- frontend/src/common/api.js
+++ frontend/src/common/api.js
@@ -1,6 +1,6 @@
 const DEVELOP = process.env.REACT_APP_DEVELOP
 let baseName = ""
-if (DEVELOP === true) {
+if (DEVELOP === "true") {
   const HOST = process.env.REACT_APP_API_HOST
   const PORT = process.env.REACT_APP_API_PORT
   baseName = `http://${HOST}:${PORT}`
