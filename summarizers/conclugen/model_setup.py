283aeb5f93921440a2899dd5a7617057ba81f76e
Dominik Schwabe <schwabedominik@gmail.com>
2022-04-04 12:06:06 +0200

    fix(conclugen): remove invalid os.remove

--- summarizers/conclugen/model_setup.py
+++ summarizers/conclugen/model_setup.py
@@ -13,14 +13,12 @@ MODEL_NAME = "Webis-Conclugen21"
 def setup():
     # create checkpoints directory if non-existent
     print("Creating and downloading checkpoints")
-    pathlib.Path(SAVE_PATH).mkdir(parents=True, exist_ok=True)
+    SAVE_PATH.mkdir(parents=True, exist_ok=True)
     response = requests.get(URL, stream=True)
     logger = logging.getLogger(inspect.currentframe().f_code.co_name)
     logger.info("Downloading %s", MODEL_NAME)
     file = tarfile.open(fileobj=response.raw, mode="r|gz")
     file.extractall(path=SAVE_PATH)
-    os.remove(SAVE_PATH / URL.split("/")[-1])
-    print("Deleted compressed file.")
     logger.info("Done")
 
 if __name__ =="__main__":
