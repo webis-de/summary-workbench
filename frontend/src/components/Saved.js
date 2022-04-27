7424cd9629409e7c97d96247282353bed4b0c6e9
Dominik Schwabe <schwabedominik@gmail.com>
2022-04-27 12:07:19 +0200

    feat(saved calculation): delete confirmation

--- frontend/src/components/Saved.js
+++ frontend/src/components/Saved.js
@@ -4,34 +4,53 @@ import { FaQuestionCircle } from "react-icons/fa";
 import { HoverProvider } from "../contexts/HoverContext";
 import { ScoreWorkbench } from "./ScoreWorkbench";
 import { Badge, BadgeGroup } from "./utils/Badge";
-import { DeleteButton } from "./utils/Button";
+import { Button, DeleteButton } from "./utils/Button";
 import { Disclosure, DisclosureContent, DisclosureToggle } from "./utils/Disclosure";
+import { Modal, ModalTitle, useModal } from "./utils/Modal";
 import { HeadingBig, HeadingMedium } from "./utils/Text";
 import { Tooltip } from "./utils/Tooltip";
 
 const SavedEntry = ({ calculation, deleteCalculation }) => {
   const { id, metrics } = calculation;
+  const [isOpen, openModal, closeModal] = useModal();
   return (
     <div className="shadow-md rounded-lg">
+      <Modal isOpen={isOpen} close={closeModal}>
+        <div className="p-4 max-w-[400px] divide-y">
+          <div className="whitespace-nowrap overflow-hidden overflow-ellipsis">
+            Delete {'"'}
+            {calculation.id}
+            {'"'}?
+          </div>
+          <div className="pt-2 flex justify-end gap-4">
+            <Button variant="success" appearance="soft" onClick={deleteCalculation}>
+              Yes
+            </Button>
+            <Button variant="danger" appearance="soft" onClick={closeModal}>
+              No
+            </Button>
+          </div>
+        </div>
+      </Modal>
       <HoverProvider>
         <Disclosure>
           <div className="border border-black rounded-lg divide-y divide-gray-300">
-            <DisclosureToggle>
-              <div className="px-4 h-12 flex justify-between items-center w-full">
-                <HeadingMedium raw>{id}</HeadingMedium>
-                <BadgeGroup>
-                  {metrics.map((metric, i) => (
-                    <Badge key={i}>{metric}</Badge>
-                  ))}
-                </BadgeGroup>
-              </div>
-            </DisclosureToggle>
+            <div className="flex items-center px-4 gap-4">
+              <DisclosureToggle>
+                <div className="h-12 flex justify-between items-center w-full">
+                  <HeadingMedium raw>{id}</HeadingMedium>
+                  <BadgeGroup>
+                    {metrics.map((metric, i) => (
+                      <Badge key={i}>{metric}</Badge>
+                    ))}
+                  </BadgeGroup>
+                </div>
+              </DisclosureToggle>
+              <DeleteButton onClick={openModal} />
+            </div>
             <DisclosureContent>
               <div className="p-4">
-                <ScoreWorkbench
-                  calculation={calculation}
-                  RightToken={<DeleteButton onClick={deleteCalculation} />}
-                />
+                <ScoreWorkbench calculation={calculation} />
               </div>
             </DisclosureContent>
           </div>
@@ -50,9 +69,9 @@ const Saved = ({ calculations, deleteCalculation }) => (
       </Tooltip>
     </div>
     <div className="flex flex-col gap-2">
-      {calculations.map((calculation, index) => (
+      {calculations.map((calculation) => (
         <SavedEntry
-          key={index}
+          key={calculation.id}
           calculation={calculation}
           deleteCalculation={() => deleteCalculation(calculation.id)}
         />
--- frontend/src/components/utils/Modal.js
+++ frontend/src/components/utils/Modal.js
@@ -12,13 +12,18 @@ const useModal = () => {
   return [isOpen, open, close];
 };
 
-const Modal = ({ children, isOpen, close }) => (
-  <Dialog open={isOpen} onClose={close}>
-    <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-    <div className="fixed bg-white shadow-xl shadow-stone-400 z-50 border inset-x-12 inset-y-6 overflow-y-auto bg-slate">
-      {children}
-    </div>
-  </Dialog>
-);
+const Modal = ({ children, isOpen, close, fit }) => {
+  const fitClass = fit ? "inset-x-12 inset-y-6" : "top-1/2 left-1/2 transform -translate-x-1/2 -translate-x-1/2";
+  return (
+    <Dialog open={isOpen} onClose={close}>
+      <Dialog.Overlay className="fixed inset-0 bg-black opacity-30 z-20" />
+      <div
+        className={`${fitClass} fixed bg-white shadow-xl shadow-stone-400 z-50 border overflow-y-auto bg-slate`}
+      >
+        {children}
+      </div>
+    </Dialog>
+  );
+};
 
 export { Modal, ModalTitle, useModal };
