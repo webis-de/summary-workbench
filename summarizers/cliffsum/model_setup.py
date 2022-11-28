bc8ba6dc89b01e0a577127a2efd00202bdbd6b19
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 23:02:11 +0000

    Adding tarfile member sanitization to extractall()

--- summarizers/cliffsum/model_setup.py
+++ summarizers/cliffsum/model_setup.py
@@ -42,7 +42,26 @@ def setup():
     logging.info("Downloading %s checkpoints", MODEL)
     response = requests.get(url, stream=True)
     with tarfile.open(fileobj=response.raw, mode="r|gz") as file:
-        file.extractall(path=SAVE_DIR)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(file, path=SAVE_DIR)
     logging.info("Done")
 
 
