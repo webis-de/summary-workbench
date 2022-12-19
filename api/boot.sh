a3435089df5737cc131ec22bd27a4fe5970b2184
Dominik Schwabe <schwabedominik@gmail.com>
2022-12-19 14:31:43 +0100

    chore(deps): update api and longformer2roberta dependencies

--- api/boot.sh
+++ api/boot.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 export VIRTUAL_ENV=/root/.venv
-[[ -f $VIRTUAL_ENV/bin/activate ]] || python -m venv $VIRTUAL_ENV || exit 1
+if [[ ! -r $VIRTUAL_ENV/bin/python ]] || [[ ! -f $VIRTUAL_ENV/bin/activate ]]; then
+  echo "no valid virtualenv found, creating ..."
+  rm -rf $VIRTUAL_ENV
+  python -m venv $VIRTUAL_ENV || exit 1
+fi
 source $VIRTUAL_ENV/bin/activate || exit 1
 
 cd /app || exit 1
